605bbb1665cc8a245c78157a5054b0abeddeb436
Corey Farwell <coreyf@rwell.org>
2015-03-28 23:49:22 -0400

    Update rust feature flags

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#![feature(io)]
+#![feature(os,old_io)]
 
 use std::old_io::process::{Command, ProcessExit, StdioContainer};
 use std::os;
