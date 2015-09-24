47554ac229c96d06898c7bf30536b5d0e1757e42
Vladimir Vukicevic <vladimir@pobox.com>
2015-09-23 23:13:31 -0400

    win32 build fixes

--- build.rs
+++ build.rs
@@ -2,10 +2,18 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+extern crate pkg_config;
+
 use std::env;
 use std::process::Command;
 
 fn main() {
+    if !env::var("TARGET").unwrap().contains("eabi") &&
+        pkg_config::Config::new().atleast_version("16.0.10").find("freetype2").is_ok()
+    {
+        return;
+    }
+
     let make = env::var("MAKE").unwrap_or("make".to_string());
     let result = Command::new(make)
         .args(&["-R", "-f", "makefile.cargo"])
